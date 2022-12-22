b658d671b2b4be5e96bdbf2ce522c87406fbc61e
Valentin DLP <valentindlp@live.fr>
2022-12-22 15:47:30 +0100

    update models based on latest json schema

--- models/generated/airbyte_incremental/public/botify_visits_report.sql
+++ models/generated/airbyte_incremental/public/botify_visits_report.sql
@@ -1,36 +1,36 @@
 {{ config(
-    indexes = [{'columns':['_airbyte_unique_key'],'unique':True}],
-    unique_key = "_airbyte_unique_key",
+    indexes = [{'columns':['_airbyte_emitted_at'],'type':'btree'}],
+    unique_key = '_airbyte_ab_id',
     schema = "public",
     tags = [ "top-level" ]
 ) }}
 -- Final base SQL model
--- depends_on: {{ ref('botify_visits_report_scd') }}
+-- depends_on: {{ ref('botify_visits_report_ab3') }}
 select
-    _airbyte_unique_key,
     url,
     {{ adapter.quote('date') }},
+    host,
+    {{ adapter.quote('path') }},
     uuid,
-    hostname,
-    newusers,
+    device,
+    medium,
+    {{ adapter.quote('source') }},
     sessions,
     url_hash,
     is_golden,
-    conversions,
-    landingpage,
-    sessionmedium,
-    sessionsource,
-    devicecategory,
+    new_users,
+    page_views,
+    data_source,
+    sync_timestamp,
     engagedsessions,
-    screenpageviews,
-    userengagementduration,
+    session_duration,
+    goal_completions_all,
     _airbyte_ab_id,
     _airbyte_emitted_at,
     {{ current_timestamp() }} as _airbyte_normalized_at,
     _airbyte_botify_visits_report_hashid
-from {{ ref('botify_visits_report_scd') }}
+from {{ ref('botify_visits_report_ab3') }}
 -- botify_visits_report from {{ source('public', '_airbyte_raw_botify_visits_report') }}
 where 1 = 1
-and _airbyte_active_row = 1
 {{ incremental_clause('_airbyte_emitted_at', this) }}
 
